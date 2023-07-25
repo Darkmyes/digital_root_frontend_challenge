9ff413a18b382e39cf3c304ffaeaf5bc2988d3e8
Darkmyes <darkmyes@gmail.com>
2023-07-25 17:43:28 -0500

    SomePage added to mockup the flights url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ResultsView from '../views/ResultsView.vue'
+import SomePageView from '../views/SomePageView.vue'
 import Error404View from '../views/Error404View.vue'
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/search_results',
       name: 'search_results',
       component: ResultsView
+    },
+    {
+      path: '/some_page',
+      name: 'some_page',
+      component: SomePageView
     },
     {
       path: '/:pathMatch(.*)',
---
+++ src/views/SomePageView.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="d-flex flex-column align-center justify-center bg-primary text-white" style="min-height: 100%;">
+      <v-icon size="64">mdi-web</v-icon>
+      <h1>Some Page to redirect to buy the Flight</h1>
+    </main>
+</template>
