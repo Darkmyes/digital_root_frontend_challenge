6e12a67b4ac94a3cc5f536559921011298353b7e
Darkmyes <darkmyes@gmail.com>
2023-07-25 17:41:02 -0500

    404NotFound Page added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ResultsView from '../views/ResultsView.vue'
+import Error404View from '../views/Error404View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'search_results',
       component: ResultsView
     },
+    {
+      path: '/:pathMatch(.*)',
+      name: '404',
+      component: Error404View
+    }
   ]
 })
 
---
+++ src/views/Error404View.vue
@@ -0,0 +1,10 @@
+<template>
+    <main class="d-flex flex-column align-center justify-center bg-primary text-white" style="min-height: 100%;">
+      <v-icon size="64">mdi-alert</v-icon>
+      <h1>Page Not Found</h1>
+  
+      <v-btn color="white" class="mt-4 text-primary" href="/" prepend-icon="mdi-home" size="large">
+        Return to Home
+      </v-btn>
+    </main>
+</template>
