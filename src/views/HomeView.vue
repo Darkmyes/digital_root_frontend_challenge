a2cd3a022f92814479c509920f23e61373013191
Darkmyes <darkmyes@gmail.com>
2023-07-25 17:09:01 -0500

    Home View Modified & extra unused components deleted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="pa-4 pa-sm-8">
+    <div class="mb-4 text-center d-flex flex-column align-center justify-center text-secondary">
+      <v-img src="/plane-logo.png" :width="200" alt="Web Logo"></v-img>
+      <h1 class="text-primary"><b>Flights Finder</b></h1>
+      <p>
+        <b>Unleash Your Wanderlust! Your Ultimate Flight Finder for Soaring Adventures Worldwide.</b>
+      </p>
+      <p>
+        <b>Discover the Cheapest Deals, Hottest Destinations, and Smoothest Takeoffs with Just One Click!</b>
+      </p>
+    </div>
+    
   </main>
 </template>
