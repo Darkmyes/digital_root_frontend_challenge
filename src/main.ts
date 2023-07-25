b21b627872c40c1f5ef687b1575d7b6770f2908b
Darkmyes <darkmyes@gmail.com>
2023-07-25 16:51:01 -0500

    vuetify added

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,48 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    theme: {
+        defaultTheme: 'mainTheme',
+        themes: {
+            mainTheme: {
+                dark: false,
+                colors: {
+                    background: '#FFFFFF',
+                    surface: '#FFFFFF',
+                    primary: '#29aae1',
+                    'primary-darken-1': '#29aae1',
+                    secondary: '#2b3d91',
+                    'secondary-darken-1': '#2b3d91',
+                    error: '#B00020',
+                    info: '#2196F3',
+                    success: '#4CAF50',
+                    warning: '#FB8C00',
+                },
+            }
+        }
+    },
+})
+
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
